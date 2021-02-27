4a9501081abb834db5756b8866f9c22695283973
Snoz <snoz.andrey94@mail.ru>
2021-02-27 15:22:30 +0300

    avatar link validation

--- backend/app.js
+++ backend/app.js
@@ -36,7 +36,7 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string(),
+    avatar: Joi.string().regex(/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?$/),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(3),
   }).unknown(true),
